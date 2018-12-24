e7fadd9dac5c88f7c3c4fd9e4cda8169637a4dad
olektomek <olekstomek@gmail.com>
2018-12-24 11:57:20 +0100

    add some improvements
    - add many formats file (raster graphisc)
    - the ability to transfer found files
    - information about copied files, found files, etc
    - autoclose files with 'with open'

--- main.py
+++ main.py
@@ -1,22 +1,58 @@
-import shutil, os, string, random
+import os, shutil, string, random
 
-def generate(size=5, chars=string.ascii_uppercase + string.digits):
+MOVE_FILES = False
+
+def generate(size=9, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-SELECTED_PHOTOS = 'selected_photos-' + generate()
-root = r'%s' % os.getcwd().replace('\\','/')
-print(root)
+dir = '.'
+formats = ('.jpeg', '.jps', '.jpg', '.jpeg 2000', '.djvu', '.tiff', '.png', '.gif', '.bmp', '.flif', '.xcf', '.xpm', '.psd')
+print('Search for file formats: ')
+print(formats)
+
+content = os.listdir(dir)
+print('\nList of found files in directory with script: ')
+print(content)
+print('The number of files found: {}'.format(len(content)))
 
 with open('IWantThisPicture.txt', 'r') as source:
     files = source.read().split()
-source.close()
 
+print('\nFiles to search for: ')
+print(files)
+
+content = [filename.lower() for filename in content]
+list_of_files = [i for i in content if (os.path.isfile(i) and i.endswith(formats))]
+print('List of found files with searched extensions: ')
+print(list_of_files)
+print('The number of files found with searched extensions: {}\n'.format(len(list_of_files)))
+
+list_of_files_with_found_files_by_name = []
+counter_of_searches = 0
+for item in list_of_files:
+    for item2 in files:
+        if item2 in item:
+            print('Found ' + item)
+            list_of_files_with_found_files_by_name.append(item)
+            counter_of_searches += 1
+        else:
+            continue
+
+print('\nCopy {} from {} (all files in directory)'.format(counter_of_searches, len(list_of_files)))
+print('Found: {}/{} files in directory from text file'.format(counter_of_searches, len(files)))
+
+SELECTED_PHOTOS = 'selected_photos-' + generate()
 try:
     os.makedirs(SELECTED_PHOTOS)
 except:
     print('An error with the action of creating a folder')
 
-for f in files:
-    path = root + '/' + f
-    print('Selected file: ' + path)
-    shutil.copy(f + '.PNG', SELECTED_PHOTOS)
+for file in list_of_files_with_found_files_by_name:  
+    if MOVE_FILES:
+        print('Move now ' + file + " to " + SELECTED_PHOTOS)  
+        shutil.move(os.path.join(dir, file), os.path.join(SELECTED_PHOTOS, file))
+    else:
+        print('Copy now ' + file + " to " + SELECTED_PHOTOS)  
+        shutil.copy(os.path.join(dir, file), os.path.join(SELECTED_PHOTOS, file))
+
+os.startfile(SELECTED_PHOTOS)
